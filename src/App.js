3b3017e219b1a5538395266c9b630429f4ea6135
Yahya <yahyanaeem2017@gmail.com>
2022-02-08 09:26:18 +0500

    add existing file into git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const date = new Date();
+  // const state = useState();
+  const [count, stateposition] = useState(0);
+  let incNum = () => {
+    stateposition(count + 1);
+  };
+
+  let decNum = () => {
+    stateposition(count - 1);
+  };
+
+  if (count < 0) {
+    alert("not decrease by 0");
+  }
+  // stateposition();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>{count}</h1>
+        <br />
+        <button onClick={incNum}>Increment</button>
+        <button onClick={decNum}>decrement</button>
+      </div>
     </div>
   );
 }
